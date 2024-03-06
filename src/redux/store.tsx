ed4367b0ca2928352f4ef74c1f775c99aba622c2
Tonion <thanhnguyen240202@gmail.com>
2024-03-07 03:35:39 +0700

    save auth to localStorage

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,20 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import todoListReducer from "../components/Todolist/todoListSlice"
 import i18nReducer from "../i18n/i18nSlice";
 import filterReducer from "../components/Filter/filterSlice";
 import authReducer from "../components/Login/authSlice"
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['auth']
+}
+
+
+const rootReducer = combineReducers({
+    todoList: todoListReducer,
+    i18n: i18nReducer,
+    filter: filterReducer,
+    auth: authReducer
+})
+
+const persistedAuthReducer = persistReducer(persistConfig, rootReducer) 
 
 const store = configureStore({
-    reducer: {
-        todoList: todoListReducer,
-        i18n: i18nReducer,
-        filter: filterReducer,
-        auth: authReducer
-    }
+    reducer: persistedAuthReducer
 })
+
+// const store = configureStore({
+//     reducer: {
+//         todoList: todoListReducer,
+//         i18n: i18nReducer,
+//         filter: filterReducer,
+//         auth: authReducer
+//     }
+// })
 
 export type RootState = ReturnType<typeof store.getState>
 
 export type AppDispatch = typeof store.dispatch
 
 export default store
+
+export const persistor = persistStore(store)
